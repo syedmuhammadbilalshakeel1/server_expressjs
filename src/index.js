b5d190562069bb5d7b224a904d80002613123622
syed muhammad bilal <syedmuhammadbilal638@gmail.com>
2023-01-22 17:54:24 +0500

    created the post request to the server and also send the encoded message from the data from the server

--- src/index.js
+++ src/index.js
@@ -2,20 +2,40 @@ const express = require("express");
 
 const app = express();
 const PORT = 3001;
+// here we applay some middle where function that perform some task example is logging some time
+app.use(express.json());
+// that is the encoded data that is come from the data postman
+app.use(express.urlencoded());
 
+
+// that is the port were the server is runing on this
 app.listen(PORT, () =>
   console.log(`Running express server at thie port ${PORT}`)
 );
 
+// THAT IS THE GET DATA FROM THE DATA THAT IS WRITTEN IN THE DYNAMICALLY
+
+const userData = [
+  {
+    name: "taha",
+    email: "taha@gmail.com",
+  },
+  {
+    name: "bilal",
+    email: "bilal@gmail.com",
+  },
+];
 app.get('/data', (req, resporance) => {
-  resporance.send([
-    {
-      name: 'taha',
-      email: 'taha@gmail.com',
-    },
-     {
-      name: 'bilal',
-      email: 'bilal@gmail.com',
-    },
-  ]);
+  resporance.send(userData);
 });
+
+
+// that is the post request from the post to get data server
+// request.body is that there all data is given
+app.post('/data', (req, res) => {
+    
+    console.log(req.body);
+    userData.push(req.body);
+    res.send(201);
+
+})
